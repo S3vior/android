01612cba784f56fbfd726fc2866db3fe834de3b3
Khater <hamdikhater68@gmail.com>
2022-12-05 00:56:32 +0200

    navGraph

--- app/src/main/java/com/example/s3vior/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/s3vior/ui/activity/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.s3vior.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.s3vior.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 }
--- app/src/main/java/com/example/s3vior/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/example/s3vior/ui/fragment/main/MainFragment.kt
@@ -6,69 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.s3vior.R
 import com.example.s3vior.databinding.FragmentMainBinding
-import com.example.s3vior.ui.fragment.bottomFragment.AnnouncementFragment
-import com.example.s3vior.ui.fragment.bottomFragment.HomeFragment
-import com.example.s3vior.ui.fragment.bottomFragment.MapsFragment
-import com.example.s3vior.ui.fragment.bottomFragment.MoreFragment
 
 class MainFragment : Fragment() {
-    private lateinit var binding:FragmentMainBinding
-    private val announcementFragment=AnnouncementFragment()
-    private val homeFragment=HomeFragment()
-    private val mapsFragment=MapsFragment()
-    private val moreFragment= MoreFragment()
+
+
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View  {
+    ): View {
 
-        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
 
-        initSubView()
-        addNavigationListener()
         return binding.root
     }
 
-    private fun addFragment(fragment: Fragment) {
-        val transactions = requireActivity().supportFragmentManager.beginTransaction()
-        transactions.add(R.id.fragment_container, fragment)
-        transactions.commit()
-    }
-    private fun replaceFragment(fragment: Fragment) {
-        val transactions = requireActivity().supportFragmentManager.beginTransaction()
-        transactions.replace(R.id.fragment_container, fragment)
-        transactions.commit()
+    override fun onResume() {
+        super.onResume()
+        val navController = activity?.findNavController(R.id.fragment_host)
+        binding.bottomNavigationView.setupWithNavController(navController!!)
     }
 
-    private fun initSubView() {
-        addFragment(homeFragment)
-    }
 
-    private fun addNavigationListener() {
-        binding.bottomNavigationView.setOnItemSelectedListener  { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    replaceFragment(homeFragment)
-                    true
-                }
-                R.id.report ->{
-                    replaceFragment(announcementFragment)
-                    true
-                }
-                R.id.more -> {
-                    replaceFragment(moreFragment)
-                    true
-                }
-                R.id.map ->{
-                    replaceFragment(mapsFragment)
-                    true
-
-                }
-                else -> false
-            }
-
-        }
-    }
 }
