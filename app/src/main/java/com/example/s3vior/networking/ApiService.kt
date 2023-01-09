d93be3efaee65495edce5b580e92694d12737f7b
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2023-01-09 22:08:35 +0200

    Created Post Api service

--- app/src/main/java/com/example/s3vior/networking/ApiService.kt
+++ app/src/main/java/com/example/s3vior/networking/ApiService.kt
@@ -1,12 +1,18 @@
 package com.example.s3vior.networking
 
  import com.example.s3vior.ui.recyclerView.Person
+ import com.google.gson.JsonObject
  import retrofit2.Call
+ import retrofit2.http.Body
  import retrofit2.http.GET
+ import retrofit2.http.POST
 
 interface PersonApiService {
  @GET("persons")
   fun getAllPersons(): Call<List<Person>>
+
+ @POST("persons")
+ fun insertPerson(@Body person: JsonObject): Call<List<Person>>
 }
 
 
