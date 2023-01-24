30e7742c74a58de0148f794f63b41e0b265ec8a4
eslamahemed <proislamahmed74@gmail.com>
2023-01-24 18:40:38 +0200

    first push

--- app/src/main/java/com/example/s3vior/networking/PersonApiService.kt
+++ app/src/main/java/com/example/s3vior/networking/PersonApiService.kt
@@ -1,17 +1,22 @@
 package com.example.s3vior.networking
 
- import com.example.s3vior.ui.recyclerView.Person
- import retrofit2.Call
- import retrofit2.Response
- import retrofit2.http.GET
- import retrofit2.http.POST
+import com.example.s3vior.ui.recyclerView.Person
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface PersonApiService {
- @GET("persons")
-  suspend fun getAllPersons(): Response<List<Person>>
+    @GET("persons")
+    suspend fun getAllPersons(): Response<List<Person>>
 
- @POST("persons")
- suspend fun sendAllPersons(): Response<List<Person>>
+    @Multipart
+    @POST("persons")
+    suspend fun uploadCase(
+
+    ): Response<List<Person>>
 }
 
 
