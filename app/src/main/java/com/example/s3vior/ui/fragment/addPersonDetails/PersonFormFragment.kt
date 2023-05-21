5b5dc01da37cc8e7a6f05a0ba66f97055fe4a83e
Khater <hamdikhater68@gmail.com>
2023-05-21 16:17:04 +0300

    handle progress bar on change password and contact us and upload person and each required text input , validate email and name

--- app/src/main/java/com/example/s3vior/ui/fragment/addPersonDetails/PersonFormFragment.kt
+++ app/src/main/java/com/example/s3vior/ui/fragment/addPersonDetails/PersonFormFragment.kt
@@ -2,22 +2,18 @@ package com.example.s3vior.ui.fragment.addPersonDetails
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.app.Dialog
 import android.util.Log
 import android.view.View
-import android.view.Window
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.example.s3vior.R
 import com.example.s3vior.databinding.FragmentPersonFormBinding
 import com.example.s3vior.ui.fragment.base.BaseFragment
+import com.example.s3vior.utils.BtnLoadingProgressbar
 import com.example.s3vior.viewModel.SharedViewModel
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -29,6 +25,7 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
 
 
     private lateinit var status: String
+    private val handler = android.os.Handler()
 
     @SuppressLint("UseRequireInsteadOfGet", "SetTextI18n")
 
@@ -60,16 +57,16 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
 
     }
 
-    private fun sendPersonData() {
+    private fun sendPersonData(view: View) {
 
         try {
             val imageUri = sharedViewModel.thirdDetails.value?.imageUri
-            binding.progressBar.visibility = View.VISIBLE
-            binding.button4.visibility = View.GONE
+            val progressbar = BtnLoadingProgressbar(view) // `it` is view of button
+            progressbar.setLoading()
             lifecycleScope.launch(Dispatchers.IO) {
 
                 val result = sharedViewModel.uploadPersonUseCase.invoke(
-                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NDU3NjI5MiwianRpIjoiYmE2OGNmZDktYTQ4NC00YTE3LWE1OTUtNGI5Y2E4MGY0MWY5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjg0NTc2MjkyLCJleHAiOjE2ODQ1OTQyOTJ9.B00MpP9tuEZWXBLkyCqjDvO8Byxwe2ohadrYz--UqlQ",
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NDY2MzI0MywianRpIjoiNDQ4ZGM4YmEtMWMwMy00NGEzLWJlMjQtZDEyNGRmMmYxZjViIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjg0NjYzMjQzLCJsb2dnZWRfb3V0IjpmYWxzZX0.WYQ2RGpVCvtkPatmB_fVkIz2XEkJYIZfkfXkVScJmlY",
                     name = binding.personName.text.toString(),
                     age = binding.personAge.text.toString().toInt(),
                     gender = binding.personGender.text.toString(),
@@ -82,10 +79,25 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
 
                 withContext(Dispatchers.Main) {
                     //     showSnackBar(result)
-                    showDefaultDialog(result)
-                    if (result == "Person uploaded" || result == "Person not uploaded") {
-                        binding.progressBar.visibility = View.GONE
-                        binding.button4.visibility = View.VISIBLE
+//                    showDefaultDialog(result)
+                    if (result == "Person uploaded") {
+                        progressbar.setState(true) {
+
+                            progressbar.reset()
+                            lifecycleScope.launch {
+                                delay(1500)
+                            }
+                            Navigation.findNavController(requireView())
+                                .navigate(R.id.action_personFormFragment_to_announcementFragment)
+
+                        }
+
+
+                    } else {
+                        startError(progressbar)
+                    }
+                    if (result == "Person not uploaded") {
+                        startError(progressbar)
                     }
 
                 }
@@ -93,11 +105,8 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
             }
 
         } catch (e: Exception) {
-
             showDefaultDialog(e.message.toString())
         }
-
-
     }
 
     private fun showDefaultDialog(message: String) {
@@ -108,14 +117,16 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
 
         alertDialogBuilder.setPositiveButton("تم") { dialog, which ->
             dialog.dismiss()
-           Navigation.findNavController(requireView()).navigate(R.id.action_personFormFragment_to_announcementFragment)
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_personFormFragment_to_announcementFragment)
 
         }
 
         alertDialogBuilder.setNegativeButton("ارسال من جديد") { dialog, which ->
 
             dialog.dismiss()
-            Navigation.findNavController(requireView()).navigate(R.id.action_personFormFragment_to_addNameFragment)
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_personFormFragment_to_addNameFragment)
 
         }
 
@@ -131,22 +142,24 @@ class PersonFormFragment : BaseFragment<FragmentPersonFormBinding>(
     override fun callFunctions() {
 
         observeDataFromViewModel()
-        binding.progressBar.visibility = View.GONE
+
         binding.button4.setOnClickListener {
-            sendPersonData()
+            sendPersonData(it)
         }
     }
 
-    private fun showSnackBar(message: String) {
-        val snackBar = Snackbar.make(
-            requireActivity().findViewById(android.R.id.content),
-            message,
-            Snackbar.LENGTH_LONG,
-        )
-        // Set the max lines of SnackBar
-        snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines =
-            10
-        snackBar.show()
+    private fun startError(progressbar: BtnLoadingProgressbar) {
+        progressbar.reset()
+        handler.postDelayed({
+            progressbar.setLoading()
+            handler.postDelayed({
+                progressbar.setState(false) { // executed after animation end
+                    handler.postDelayed({
+                        progressbar.reset()
+                    }, 1500)
+                }
+            }, 2000)
+        }, 600)
     }
 
 
--- app/src/main/java/com/example/s3vior/ui/fragment/navigationBottomFragment/moreFragment/settingsItems/password/ChangePasswordFragment.kt
+++ app/src/main/java/com/example/s3vior/ui/fragment/navigationBottomFragment/moreFragment/settingsItems/password/ChangePasswordFragment.kt
@@ -1,6 +1,7 @@
 package com.example.s3vior.ui.fragment.navigationBottomFragment.moreFragment.settingsItems.password
 
 import android.app.AlertDialog
+import android.os.Handler
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.viewModels
@@ -9,9 +10,11 @@ import com.example.s3vior.data.source.remote.endPoints.PasswordChangeRequest
 import com.example.s3vior.databinding.FragmentChangePasswordBinding
 import com.example.s3vior.domain.usecases.VALID_PASSWORD_REGEX
 import com.example.s3vior.ui.fragment.base.BaseFragment
+import com.example.s3vior.utils.BtnLoadingProgressbar
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -19,48 +22,42 @@ import kotlinx.coroutines.withContext
 @AndroidEntryPoint
 class ChangePasswordFragment :
     BaseFragment<FragmentChangePasswordBinding>(FragmentChangePasswordBinding::inflate) {
+    private val handler = Handler()
 
     private val changePasswordViewModel: ChangePasswordViewModel by viewModels()
     override fun callFunctions() {
+        emailFocusListener()
+
+        binding.activityMainBtn.setOnClickListener {
 
 
-        binding.progressBar.visibility = View.GONE
-        binding.changePassword.visibility = View.VISIBLE
-
-        binding.changePassword.setOnClickListener {
-
-
-            if (binding.oldpassword.text.isBlank()) {
-                showDefaultDialog("please, enter old password")
+            if (binding.oldpassword.text!!.isBlank()) {
+       //         showDefaultDialog("please, enter old password")
             }
 
-            if (binding.newpassword.text.toString() != binding.confirmnewpassword.text.toString()) {
 
-                showDefaultDialog("your password is not matched")
 
-            }
 
-            if (!binding.newpassword.text.matches(
+
+            if (!binding.newpassword.text!!.matches(
                     VALID_PASSWORD_REGEX
-                ) && !binding.confirmnewpassword.text.matches(VALID_PASSWORD_REGEX)
+                ) && !binding.confirmnewpassword.text!!.matches(VALID_PASSWORD_REGEX)
             ) {
-                showDefaultDialog("please, enter valid password")
+//                showDefaultDialog("please, enter valid password")
             }
 
-            if (binding.oldpassword.text.isNotBlank() && binding.newpassword.text.toString() == binding.confirmnewpassword.text.toString()
-                && binding.newpassword.text.matches(
+            if (binding.oldpassword.text!!.isNotBlank() && binding.newpassword.text.toString() == binding.confirmnewpassword.text.toString()
+                && binding.newpassword.text!!.matches(
                     VALID_PASSWORD_REGEX
-                ) && binding.confirmnewpassword.text.matches(VALID_PASSWORD_REGEX)
+                ) && binding.confirmnewpassword.text!!.matches(VALID_PASSWORD_REGEX)
             ) {
-
-
-                binding.progressBar.visibility = View.VISIBLE
-                binding.changePassword.visibility = View.GONE
+                val progressbar = BtnLoadingProgressbar(it) // `it` is view of button
+                progressbar.setLoading()
 
                 lifecycleScope.launch(Dispatchers.IO) {
                     try {
                         val result = changePasswordViewModel.changePasswordUseCase.invoke(
-                            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NDQyNzA1NSwianRpIjoiOTdmN2ZmNTctMWU2Ny00MWQxLTkxZDEtNWI3YWMxMDYzZjhlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjg0NDI3MDU1LCJleHAiOjE2ODQ0NDUwNTV9.hVXgCf_X1sFOTUVxzSqh4E3lqoiy6ayuHKCy6EYEKWM",
+                            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NDY2MzI0MywianRpIjoiNDQ4ZGM4YmEtMWMwMy00NGEzLWJlMjQtZDEyNGRmMmYxZjViIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjg0NjYzMjQzLCJsb2dnZWRfb3V0IjpmYWxzZX0.WYQ2RGpVCvtkPatmB_fVkIz2XEkJYIZfkfXkVScJmlY",
                             PasswordChangeRequest(
                                 binding.oldpassword.text.toString(),
                                 binding.newpassword.text.toString(),
@@ -68,10 +65,19 @@ class ChangePasswordFragment :
                             )
                         )
                         withContext(Dispatchers.Main) {
-                            showDefaultDialog(result)
-                            if (result == "Failed to updated Password" || result == "Password updated successfully.") {
-                                binding.progressBar.visibility = View.GONE
-                                binding.changePassword.visibility = View.VISIBLE
+                            //         showDefaultDialog(result)
+                            if (result == "Password updated successfully.") {
+                                progressbar.setState(true) {
+                                    lifecycleScope.launch {
+                                        delay(800)
+                                    }
+                                    progressbar.reset()
+                                }
+                            } else {
+                                startError(progressbar)
+                            }
+                            if (result == "Failed to updated Password") {
+                                startError(progressbar)
                             }
                         }
                     } catch (e: Exception) {
@@ -125,6 +131,69 @@ class ChangePasswordFragment :
         snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines =
             10
         snackBar.show()
+    }
+
+    private fun emailFocusListener() {
+        binding.oldpassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.passwordContainer.helperText = validOldPassword()
+            }
+        }
+        binding.confirmnewpassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.phoneContainer.helperText = validateMatching()
+            }
+        }
+        binding.newpassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.emailContainer.helperText = validNewPassword()
+            }
+        }
+
+    }
+
+    private fun validateMatching(): String? {
+        return if (binding.newpassword.text!!.toString() != binding.confirmnewpassword.text.toString()) {
+            "password not matched"
+        } else null
+    }
+
+    private fun validOldPassword(): String? {
+        return if (binding.oldpassword.text!!.toString().length < 3) {
+            "invalid password"
+        } else null
+    }
+
+    private fun validConfirmPassword():  String? {
+        return if (!binding.confirmnewpassword.text!!.matches(
+                VALID_PASSWORD_REGEX
+            )
+        ) {
+            "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character."
+        } else null
+    }
+
+    private fun validNewPassword(): String? {
+        return if (!binding.newpassword.text!!.matches(
+                VALID_PASSWORD_REGEX
+            )
+        ) {
+            "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character."
+        } else null
+    }
+
+    private fun startError(progressbar: BtnLoadingProgressbar) {
+        progressbar.reset()
+        handler.postDelayed({
+            progressbar.setLoading()
+            handler.postDelayed({
+                progressbar.setState(false) { // executed after animation end
+                    handler.postDelayed({
+                        progressbar.reset()
+                    }, 1500)
+                }
+            }, 2000)
+        }, 600)
     }
 }
 
