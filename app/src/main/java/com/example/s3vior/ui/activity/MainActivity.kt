f27ea5dcb976ade8a2ed4042beafba9caf3f15c0
Khater <hamdikhater68@gmail.com>
2023-05-22 17:28:16 +0300

    new recycler home item

--- app/src/main/java/com/example/s3vior/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/s3vior/ui/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.s3vior.ui.activity
 
 import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
@@ -27,8 +30,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContentView(binding.root)
         startUp()
 
